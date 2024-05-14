d5a53c64021007f1b908f196ac542be7d41e5de2
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-14 15:42:54 +0900

    deviseのemailに関する不要な設定をオーバーライド

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :introduction, length: { maximum: 200 }
   validates :external_blog_url, length: { maximum: 8000 }
+
+  # No use email
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+  end
 end
