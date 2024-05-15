c22d38213a093e0435dabdf5ac88b046ab2434a5
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-15 17:57:31 +0900

    userのnameに一意に、英数字のみ使えるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 20 },
+                   format: { with: /^[0-9a-zA-Z]+$/, message: I18n.t('activerecord.errors.user.name_format_error'), multiline: true }, uniqueness: true
   validates :introduction, length: { maximum: 200 }
   validates :external_blog_url, length: { maximum: 8000 }
 
