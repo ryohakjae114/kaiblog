0e46e6840a95f18ba06b5b94dd62622be9193530
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-16 17:12:01 +0900

    postのseedファイルを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,11 @@
 FactoryBot.define do
   factory :user do
     sequence :name do |n|
-      "hakjae_#{n}"
+      "hakjae#{n}"
     end
     password { '1234512345' }
     password_confirmation { '1234512345' }
+    introduction { '私の名前はハチェオです' }
+    external_blog_url { 'https://example.com' }
   end
 end
