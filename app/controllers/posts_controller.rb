2e73d53ab440c751036b32ecedbc15736af26a9c
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-16 17:52:41 +0900

    投稿一覧画面で1ページにつき10件表示する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.order(created_at: :desc)
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(10)
   end
 end
