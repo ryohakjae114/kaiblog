fb8de74749fcf034125020327bb15489cd66aa88
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-17 15:50:54 +0900

    itで書かれていること以上に検証してたテストを2つに分割

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -4,10 +4,17 @@ RSpec.describe 'Posts', type: :system do
   let!(:user) { create(:user) }
 
   it '投稿一覧できること' do
-    create(:post, body: '今日の天気はいい天気', user:)
+    expect(page).not_to have_css('.card')
     create_list(:post, 10, user:)
     visit root_path
     expect(page).to have_css('.card', count: 10)
+  end
+
+  it '投稿が作成日の降順に並んでいること' do
+    create(:post, body: '今日の天気はいい天気', user:)
+    create_list(:post, 10, user:)
+    puts Post.pluck(:id, :body)
+    visit root_path
     expect(page).to have_css('.pagination', count: 2)
     click_link '2', class: 'page-link', match: :first
     expect(page).to have_content('今日の天気はいい天気')
