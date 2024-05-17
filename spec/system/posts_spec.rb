e9c86cbfc8d1d99ad5613178c2aea3d75da5808a
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-17 11:30:52 +0900

    投稿一覧機能のテスト作成

---
+++ spec/system/posts_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :system do
+  let!(:user) { create(:user) }
+
+  it '投稿一覧できること' do
+    create(:post, body: '今日の天気はいい天気', user:)
+    create_list(:post, 10, user:)
+    visit root_path
+    expect(page).to have_css('.card', count: 10)
+    expect(page).to have_css('.pagination', count: 2)
+    click_link '2', class: 'page-link', match: :first
+    expect(page).to have_content('今日の天気はいい天気')
+  end
+end
