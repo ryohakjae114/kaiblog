b6cbc8a22aee95f6d84a9d7a5521191f8d6fc89c
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-17 11:30:12 +0900

    postが降順で表示されるように修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
     @posts = Post.order(created_at: :desc).page(params[:page]).per(10)
   end
