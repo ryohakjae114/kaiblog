4fd2ede9cca2432c1dd1b2f88fdda0cc4396dcbc
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-17 14:38:59 +0900

    新規投稿作成機能のテスト作成

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -9,14 +9,24 @@ RSpec.describe 'Posts', type: :system do
     visit root_path
     expect(page).to have_css('.card', count: 10)
     expect(page).to have_css('.pagination', count: 2)
-    click_link '2', class: 'page-link', match: :first
+    click_on '2', class: 'page-link', match: :first
     expect(page).to have_content('今日の天気はいい天気')
   end
 
   context 'ログイン時' do
     before do
-      sign_in :user
+      sign_in user
+    end
+
+    it '新規投稿できること' do
       visit root_path
+      click_on '新規投稿作成'
+      fill_in '本文', with: '今日の天気はいい天気だわ'
+      expect do
+        click_on '登録する'
+        expect(page).to have_content('新規登録しました')
+        expect(page).to have_content('今日の天気はいい天気だわ')
+      end.to change(user.posts, :count).by(1)
     end
   end
 end
