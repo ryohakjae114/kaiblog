f801721030f7529b3f0c1a241b91f86f0865e3bf
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-17 14:12:30 +0900

    新規投稿機能を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(created_at: :desc).page(params[:page]).per(10)
   end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_url, notice: t('controller.created')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
