df590b0f6518aa941ce6db09c26527b57423060a
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-19 13:56:12 +0900

    編集画面から投稿を編集できる

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  before_action :set_post, only: %i[edit update]
 
   def index
     @posts = Post.order(created_at: :desc).page(params[:page]).per(10)
@@ -19,7 +20,22 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to root_url, notice: t('controller.updated'), status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:body)
